7fcf8e011cb7d1260df835e723486a7ae608acc9
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-21 16:38:07 +0330

    feat(YouTube): add YouTube URL's regex
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -4,14 +4,24 @@ from pyrogram.types import Message, CallbackQuery
 from config import Config
 
 
-@Client.on_message(Config.IS_ADMIN)
+yt_regex = (
+    r'(https?://)?(www\.|m\.)?(youtube|youtu)\.(com|be)/'
+    r'(watch\?v=|embed/|v/|shorts/|)(?P<id>[a-zA-Z0-9_-]{11})'
+)
+
+
+@Client.on_message(
+    Config.IS_ADMIN
+    & filters.regex(fr"^{Config.REGEX_CMD_PREFIXES}youtube {yt_regex}$")
+)
 async def youtube_message(_: Client, message: Message):
+    vid_id = message.matches[0].group("id")
     pass
 
 
 @Client.on_callback_query(
     Config.IS_ADMIN
-    & filters.regex(r"^youtube (?P<id>\w+)(?: (?P<quality>\w+))$")
+    & filters.regex(r"^youtube (?P<id>[a-zA-Z0-9_-]{11})(?: (?P<quality>\w+))$")
 )
 async def youtube_callback(_: Client, query: CallbackQuery):
     pass
