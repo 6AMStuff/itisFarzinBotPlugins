89028cd5818127466cd8c5e01160ffe67800dd02
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-25 10:54:33 +0330

    feat(note): add `addnote`
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- helper/note.py
+++ helper/note.py
@@ -4,6 +4,9 @@ from pyrogram.types import Message
 from config import Config
 
 
+notes: dict = Config.getdata("notes") or {}
+
+
 @Client.on_message(
     Config.IS_ADMIN
     & filters.command(
@@ -12,7 +15,25 @@ from config import Config
     )
 )
 async def note_message(_: Client, message: Message):
-    pass
+    action = message.command[0]
+    match action:
+        case "addnote":
+            if len(message.command) >= 2:
+                note_name = message.command[1]
+                if len(message.command) > 2:
+                    note = message.text[len(action) + len(note_name) + 3:]
+                    notes[note_name] = note
+                    Config.setdata("notes", notes)
+                    await message.reply(f"Saved note `{note_name}`.")
+                    return
+                elif message.reply_to_message:
+                    await message.reply("Not supported for now.")
+                    return
+            await message.reply(
+                f"{Config.CMD_PREFIXES[0]}addnote [note name]"
+                + " [the note or reply to the note message]"
+            )
+            return
 
 
 __all__ = ["note_message"]
