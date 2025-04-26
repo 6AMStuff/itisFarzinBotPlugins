b070290adcbfaedbe18c8b170f13f101035cd7e9
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-26 17:43:24 +0330

    feat(whisper): add the inline query regex
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- whisper.py
+++ whisper.py
@@ -1,10 +1,16 @@
-from pyrogram import Client
+import re
+from pyrogram import Client, filters
 from pyrogram.types import InlineQuery, CallbackQuery
 
 
-@Client.on_inline_query()
+@Client.on_inline_query(
+    filters.regex(
+        r"^(?P<sentence>.+?)\s+(?P<username>@[a-zA-Z_]{3,16})$",
+        flags=re.DOTALL
+    )
+)
 async def whisper_inline(_: Client, query: InlineQuery):
-    pass
+    sentence, username = query.matches[0].groups()
 
 
 @Client.on_callback_query()
