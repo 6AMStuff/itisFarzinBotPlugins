86ce941dd020f2d7929a561d5a05a738277c20da
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-05-12 06:52:00 +0330

    fix(LastFM): add filter to handle only chosen inlines for LastFM
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- music/lastfm.py
+++ music/lastfm.py
@@ -214,7 +214,12 @@ def on_data_change():
     lastfm = set_up_lastfm()
 
 
+async def lastfm_chosen_filter(_, __, chosen: ChosenInlineResult):
+    return chosen.result_id in ("lastfm_status", "lastfm_expanded_status")
+
+
 lastfm = set_up_lastfm()
+lastfm_chosen = filters.create(lastfm_chosen_filter)
 
 
 @Client.on_inline_query(group=1)
@@ -264,7 +269,7 @@ async def lastfm_inline(_: Client, query: InlineQuery):
     )
 
 
-@Client.on_chosen_inline_result()
+@Client.on_chosen_inline_result(lastfm_chosen)
 async def lastfm_inline_result(app: Client, chosen: ChosenInlineResult):
     if isinstance(lastfm, str):
         await app.edit_inline_text(chosen.inline_message_id, lastfm)
