2fc94ae19c963c4302592dd0dd0fcfa865b71d59
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-06-27 15:29:23 +0330

    feat(throw): add throw plugin
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

---
+++ utility/throw.py
@@ -0,0 +1,38 @@
+from bot import Bot
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import Config
+
+
+emoji_list = {
+    "dice": "🎲",
+    "dart": "🎯",
+    "basketball": "🏀",
+    "football": "⚽️",
+    "bowling": "🎳",
+    "slot": "🎰",
+}
+
+
+@Bot.on_message(
+    Config.IS_ADMIN & filters.command("throw", Config.CMD_PREFIXES)
+)
+async def throw(app: Bot, message: Message):
+    emoji = message.command[1] if len(message.command) > 1 else None
+    if emoji not in emoji_list.values() and emoji not in emoji_list.keys():
+        await message.reply(
+            "The emoji should be one of the following: "
+            + ", ".join(map(lambda emoji: f"`{emoji}`", emoji_list.values()))
+            + "."
+        )
+        return
+
+    await app.send_dice(
+        message.chat.id, emoji_list[emoji] if emoji in emoji_list else emoji
+    )
+
+
+__all__ = ["throw"]
+__plugin__ = True
+__bot_only__ = False
