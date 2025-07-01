458ba79aebdf5d5401cb7e7295e4cd2c7a0927a1
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-07-01 07:56:24 +0330

    refactor(throw): put all commands in one function
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- utility/throw.py
+++ utility/throw.py
@@ -16,56 +16,27 @@ emoji_list = {
 
 
 @Bot.on_message(
-    Config.IS_ADMIN & filters.command("throw", Config.CMD_PREFIXES)
+    Config.IS_ADMIN
+    & filters.command(
+        ["throw", "dice", "dart", "basketball", "bowling", "slot"],
+        Config.CMD_PREFIXES
+    )
 )
 async def throw(app: Bot, message: Message):
-    emoji = message.command[1] if len(message.command) > 1 else None
-    if emoji not in emoji_list.values() and emoji not in emoji_list.keys():
-        await message.reply(
-            "The emoji should be one of the following: "
-            + ", ".join(map(lambda emoji: f"`{emoji}`", emoji_list.values()))
-            + "."
-        )
-        return
+    emoji = message.command[0]
+    if emoji == "throw":
+        emoji = message.command[1] if len(message.command) > 1 else None
+        if emoji not in emoji_list.values() and emoji not in emoji_list.keys():
+            await message.reply(
+                "The emoji should be one of the following: "
+                + ", ".join(map(lambda emoji: f"`{emoji}`", emoji_list.values()))
+                + "."
+            )
+            return
 
     await app.send_dice(
         message.chat.id, emoji_list[emoji] if emoji in emoji_list else emoji
     )
-
-
-@Bot.on_message(
-    Config.IS_ADMIN & filters.command("dice", Config.CMD_PREFIXES)
-)
-async def dice(app: Bot, message: Message):
-    await app.send_dice(message.chat.id, emoji_list[message.command[0]])
-
-
-@Bot.on_message(
-    Config.IS_ADMIN & filters.command("dart", Config.CMD_PREFIXES)
-)
-async def dart(app: Bot, message: Message):
-    await app.send_dice(message.chat.id, emoji_list[message.command[0]])
-
-
-@Bot.on_message(
-    Config.IS_ADMIN & filters.command("basketball", Config.CMD_PREFIXES)
-)
-async def basketball(app: Bot, message: Message):
-    await app.send_dice(message.chat.id, emoji_list[message.command[0]])
-
-
-@Bot.on_message(
-    Config.IS_ADMIN & filters.command("bowling", Config.CMD_PREFIXES)
-)
-async def bowling(app: Bot, message: Message):
-    await app.send_dice(message.chat.id, emoji_list[message.command[0]])
-
-
-@Bot.on_message(
-    Config.IS_ADMIN & filters.command("slot", Config.CMD_PREFIXES)
-)
-async def slot(app: Bot, message: Message):
-    await app.send_dice(message.chat.id, emoji_list[message.command[0]])
 
 
 __all__ = ["throw"]
