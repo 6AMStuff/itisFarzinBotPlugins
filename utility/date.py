ae9f5b97c93c7a727a585ca6ddbd0baf130ae232
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-07-26 14:26:35 +0330

    feat(date): add jalali date command
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- utility/date.py
+++ utility/date.py
@@ -1,7 +1,8 @@
-from datetime import datetime
 from bot import Bot
 from pyrogram import filters
+from datetime import datetime
 from pyrogram.types import Message
+from jdatetime import datetime as jdatetime
 
 from settings import Settings
 
@@ -14,6 +15,14 @@ async def date(_: Bot, message: Message):
     )
 
 
-__all__ = ("date",)
+@Bot.on_message(filters.command("jdate", Settings.CMD_PREFIXES))
+async def jdate(_: Bot, message: Message):
+    today = jdatetime.today().astimezone(Settings.TIMEZONE)
+    await message.reply(
+        today.strftime("Date: %A, %B %d (%d/%m/%Y)\nTime: %H:%M:%S (%Z)")
+    )
+
+
+__all__ = ("date", "jdate")
 __plugin__ = True
 __bot_only__ = False
