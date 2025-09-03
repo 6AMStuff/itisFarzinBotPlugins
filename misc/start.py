6efc811654553d771dba087fa209aeb494d6e38d
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-09-03 16:57:16 +0330

    feat(misc): add start plugin
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

---
+++ misc/start.py
@@ -0,0 +1,21 @@
+from bot import Bot
+from pyrogram import filters
+from pyrogram.types import Message, LinkPreviewOptions
+
+from settings import Settings
+
+
+@Bot.on_message(
+    Settings.IS_ADMIN & filters.command("start", Settings.CMD_PREFIXES + ["/"])
+)
+async def start(_: Bot, message: Message):
+    await message.reply(
+        "Welcome to itisFarzin's personal assistant bot!"
+        "\nYou can find the source code of me on [GitHub](https://github.com/6AMStuff/itisFarzinBot).",  # noqa: E501
+        link_preview_options=LinkPreviewOptions(is_disabled=True),
+    )
+
+
+__all__ = ("start",)
+__plugin__ = True
+__bot_only__ = False
